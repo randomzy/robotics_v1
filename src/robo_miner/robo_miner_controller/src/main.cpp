ab35fe28373c5b11539e7e9ed3bbd27d030bbd77
Martin Tanchev <m1tanchev@gmail.com>
2022-10-27 22:45:00 +0300

    fix: correct commit sha for robo_miner_controller

--- src/robo_miner/robo_miner_controller/src/main.cpp
+++ src/robo_miner/robo_miner_controller/src/main.cpp
@@ -12,7 +12,7 @@ ApplicationConfig generateConfig()
 
     config.userdata.user = "Martin Tanchev";
     config.userdata.repository = "https://github.com/randomzy/robotics_v1.git";
-    config.userdata.commitSha = "8751ac2ece8cfc9e703279617a79712b51923dab";
+    config.userdata.commitSha = "7a772b11ff1c06132eca51137a759314fe4deb79";
 
     // execution policy must be RUN_IN_DEDICATED_THREAD:
     config.ros2CommunicatorConfig.executionPolicy = ExecutionPolicy::RUN_IN_DEDICATED_THREAD;
